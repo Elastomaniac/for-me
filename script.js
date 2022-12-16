e917f4d09f565de555648fad726d8eacc8ced43e
Elastomaniac <vasyan89@gmail.com>
2022-12-16 11:16:42 +0300

    для себя

---
+++ script.js
@@ -0,0 +1,69 @@
+// let a = 2;
+// let b = 2.1;
+// if (a === Math.floor(b)) {
+//   console.log("equeal");
+// }
+// console.log(typeof []);
+
+// let str = "Текст, для проверки метода";
+// console.log(str.charAt(1));
+// console.log(str.includes("Текст,"));
+// str.repeat(2);
+
+// let result = 5 + 4;
+// switch (result) {
+//   case 10:
+//     console.log("многовато");
+//     break;
+//   case 9:
+//     console.log("вы угадали");
+//     break;
+//   case 4:
+//     console.log("мало не угадали");
+//     break;
+//   default:
+//     console.log("сочувствуем");
+// }
+// for (let i = 0; i < 11; i++) {
+//   console.log(i);
+// }
+// function nameFunc() {
+//   console.log("Наша функция");
+// }
+// nameFunc();
+// console.log('Разогреваем сковородку')
+// console.log('взфть яйцо 2 штуки')
+// console.log('Пожарить яйца')
+// console.log('Готово')
+
+// let num
+
+// function getSum() {
+//   console.log('вызов переменной внутри ф-ции', num);
+//   num = 100;
+//   console.log('вызов переменной внутри ф-ции', num);
+// }
+// getSum();
+// console.log("вызов переменной вне ф-ции", num);
+
+//callBack function
+// const message = (name, age) => {
+//   console.log(`меня зовут ${name} и мне` + age());
+// };
+// const age = () => {
+//   return 33;
+// };
+// message("Анна", age);
+
+
+// // Замыкание
+// const message = () => {
+//   let name = "Oleg";
+
+//   const messageOutput = () => {
+//     console.log("Привет " + name);
+//   };
+//   messageOutput();
+// };
+
+// message();
